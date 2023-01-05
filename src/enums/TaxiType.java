0076a28beeac22960ecae1dde6c03595b89981a4
Adilet <adiletismailov045@gmail.com>
2023-01-05 13:38:21 +0600

    added enum class const-get and set

--- src/enums/TaxiType.java
+++ src/enums/TaxiType.java
@@ -1,5 +1,33 @@
 package enums;
 
-public enum TaxiType {
+import java.math.BigDecimal;
 
+public enum TaxiType {
+    STANDART(new BigDecimal(20),new BigDecimal(130)),
+    COMFORT(new BigDecimal(25),new BigDecimal(150)),
+    BUSINESS(new BigDecimal(30),new BigDecimal(180));
+
+    private BigDecimal pricePerKm;
+    private BigDecimal priceForLanding;
+
+    TaxiType(BigDecimal pricePerKm, BigDecimal priceForLanding) {
+        this.pricePerKm = pricePerKm;
+        this.priceForLanding = priceForLanding;
+    }
+
+    public BigDecimal getPricePerKm() {
+        return pricePerKm;
+    }
+
+    public void setPricePerKm(BigDecimal pricePerKm) {
+        this.pricePerKm = pricePerKm;
+    }
+
+    public BigDecimal getPriceForLanding() {
+        return priceForLanding;
+    }
+
+    public void setPriceForLanding(BigDecimal priceForLanding) {
+        this.priceForLanding = priceForLanding;
+    }
 }
