9dbef8ebd075488a48b408135f8270439724410e
Adilet <adiletismailov045@gmail.com>
2023-01-05 18:18:27 +0600

    feature-database-driver

--- src/dao/Database.java
+++ src/dao/Database.java
@@ -1,6 +1,7 @@
 package dao;
 
 import classes.Client;
+import classes.Driver;
 import classes.Taxi;
 
 import java.util.HashSet;
@@ -9,5 +10,5 @@ import java.util.Set;
 public class Database {
     public Set<Taxi> taxis = new HashSet<>();
     public Set<Client>clientSet = new HashSet<>();
-
+    public Set<Driver> drivers = new HashSet<>();
 }
